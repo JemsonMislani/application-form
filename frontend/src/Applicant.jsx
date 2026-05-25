2b063852d880ab2934eda4b67fdbc117d53d96bd
Jemson <jemsonmislani@gmail.com>
2026-05-25 11:38:16 +0800

    import bootstrap, create a home page, create temporary data and render it to table using .map.

--- frontend/src/Applicant.jsx
+++ frontend/src/Applicant.jsx
@@ -1,8 +1,47 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 export default function Applicant(){
+    const [applicant, setApplicant] = useState([{
+        name: 'Jemson Mislani',
+        email: 'jem@gmail.com',
+        contact: '09213456789',
+        applying: 'Software Developer'
+    }])
     return(
-        <>
-            Applicant
-        </>
+<div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+            <div className="w-50 bg-white rounded p-3">
+                <button className="btn btn-success">Add +</button>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Contact Number</th>
+                            <th>Applying for</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            applicant.map((app) => {
+                        return <tr>
+                            <td>{app.name}</td>
+                            <td>{app.email}</td>
+                            <td>{app.contact}</td>
+                            <td>{app.applying}</td>
+                            <td>
+                                <button 
+                                    className='btn btn-success'>Edit</button>
+                                <button
+                                    className='btn btn-danger'>Delete</button>
+                            </td>
+                        </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 }
